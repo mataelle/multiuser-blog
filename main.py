7ece81b903220d5bc2ca930011ed696e75b41025
mataelle <mataelle@gmail.com>
2016-07-11 15:54:51 +0300

    templates added

--- main.py
+++ main.py
@@ -19,12 +19,54 @@ import jinja2
 import os
 
 templates_dir = os.path.join(os.path.dirname(__file__), 'templates')
-jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(templates_dir), autoescape=True)
+jinja_env = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(templates_dir), autoescape=True)
 
-class MainHandler(webapp2.RequestHandler):
+from google.appengine.ext import db
+
+
+class Post(db.Model):
+    subject = db.StringProperty(required=True)
+    content = db.TextProperty(required=True)
+    created = db.DateTimeProperty(auto_now_add=True)
+    last_modified = db.DateTimeProperty(auto_now=True)
+
+
+class User(db.Model):
+    username = db.StringProperty(required=True)
+    password = db.StringProperty(required=True)
+
+
+class Handler(webapp2.RequestHandler):
+
+    def render_str(self, template, **params):
+        t = jinja_env.get_template(template)
+        return t.render(params)
+
+    def render(self, template, **kw):
+        self.response.write(self.render_str(template, **kw))
+
+
+class BlogFront(Handler):
+
     def get(self):
-        self.response.write('Hello project!')
+        self.render('blog.html')
+
+
+class PostPage(Handler):
+
+    def get(self, post_id):
+        self.render('post.html')
+
+
+class NewPost(Handler):
+
+    def get(self):
+        self.render('newpost.html')
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler),
+    ('/', BlogFront),
+    ('/blog/?', BlogFront),
+    ('/blog/([0-9]+)', PostPage),
+    ('/blog/newpost', NewPost),
 ], debug=True)
